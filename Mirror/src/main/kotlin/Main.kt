e30fd9aaf03d7e399474698b0fd4e1b5482248cc
lars.gustafson@gmail.com <lars.gustafson@gmail.com>
2022-01-17 16:03:52 +0100

    Playing around with intellij project and git ignore

--- Mirror/src/main/kotlin/Main.kt
+++ Mirror/src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
+import se.fivefactorial.magicmirror.ui.MirrorUI
+
 fun main(args: Array<String>) {
     println("Hello World Mirror!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
+    var mirrorUI = MirrorUI()
+
+    println(mirrorUI.a())
+    println(mirrorUI.b())
+    println(mirrorUI.c())
+
+
 }
---
+++ UI/src/main/kotlin/se/fivefactorial/magicmirror/ui/MirrorUI.kt
@@ -0,0 +1,10 @@
+package se.fivefactorial.magicmirror.ui
+
+class MirrorUI {
+
+    fun a() = "a"
+    fun b() = "b"
+    fun c() = hidden()
+    internal fun hidden() = "hidden"
+
+}
