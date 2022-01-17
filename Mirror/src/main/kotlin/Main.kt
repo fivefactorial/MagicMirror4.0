0a31d1ee1f9d3a10a5fd6f583b23a4bd61e69931
lars.gustafson@gmail.com <lars.gustafson@gmail.com>
2022-01-17 17:18:01 +0100

    Added UI frame

--- Mirror/src/main/kotlin/Main.kt
+++ Mirror/src/main/kotlin/Main.kt
@@ -1,17 +1,6 @@
 import se.fivefactorial.magicmirror.ui.MirrorUI
 
 fun main(args: Array<String>) {
-    println("Hello World Mirror!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-
-    var mirrorUI = MirrorUI()
-
-    println(mirrorUI.a())
-    println(mirrorUI.b())
-    println(mirrorUI.c())
-
-
+    val ui = MirrorUI().apply { visible = true }
 }
--- UI/src/main/kotlin/se/fivefactorial/magicmirror/ui/MirrorUI.kt
+++ UI/src/main/kotlin/se/fivefactorial/magicmirror/ui/MirrorUI.kt
@@ -1,10 +1,58 @@
 package se.fivefactorial.magicmirror.ui
 
-class MirrorUI {
+import java.awt.Dimension
+import java.awt.GraphicsEnvironment
+import java.awt.Toolkit
+import java.awt.event.WindowEvent
+import java.awt.event.WindowListener
+import javax.swing.JFrame
+import javax.swing.SwingUtilities
 
-    fun a() = "a"
-    fun b() = "b"
-    fun c() = hidden()
-    internal fun hidden() = "hidden"
+class MirrorUI(private val onClose: () -> Any = {}) {
+
+    private val ui: JFrame = JFrame().apply {
+        val screenSize: Dimension = Toolkit.getDefaultToolkit().screenSize
+        setSize(screenSize.width / 2, screenSize.height / 2)
+        setLocation(screenSize.width / 4, screenSize.height / 4)
+        addWindowListener(object : WindowListener {
+            override fun windowOpened(e: WindowEvent?) {}
+
+            override fun windowClosing(e: WindowEvent?) {
+                stop()
+            }
+
+            override fun windowClosed(e: WindowEvent?) {}
+
+            override fun windowIconified(e: WindowEvent?) {}
+
+            override fun windowDeiconified(e: WindowEvent?) {}
+
+            override fun windowActivated(e: WindowEvent?) {}
+
+            override fun windowDeactivated(e: WindowEvent?) {}
+
+        })
+        defaultCloseOperation = JFrame.DO_NOTHING_ON_CLOSE
+    }
+
+    var visible: Boolean
+        get() = ui.isVisible
+        set(value) {
+            ui.isVisible = value
+        }
+
+    fun setFullscreen() = SwingUtilities.invokeLater {
+        ui.dispose()
+        ui.isUndecorated = true
+        val device = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
+        device.fullScreenWindow = ui
+        ui.isVisible = true
+    }
+
+    fun stop() = SwingUtilities.invokeLater {
+        visible = false
+        ui.dispose()
+        onClose()
+    }
 
 }
